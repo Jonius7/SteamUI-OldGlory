fc0f7e72739dedf0f2b2edbe14ffb0229e5086d8
Jonius7 <jhgenius01@hotmail.com>
2020-07-24 17:07:47 +1000

    Update js_tweaker.py

--- js_tweaker.py
+++ js_tweaker.py
@@ -8,7 +8,6 @@ from jsmin import jsmin
 if not os.path.isfile("libraryroot.beaut.js"):
     print("Opening JS file and beautifying...")
     library = jsbeautifier.beautify_file("libraryroot.js")
-    #library = jsbeautifier.beautify_file("librarytest.js")
 
     f = open("libraryroot.beaut.js", "wt", newline='', encoding="UTF-8")
     print("Writing beautified file... please do not close")
@@ -18,23 +17,8 @@ if not os.path.isfile("libraryroot.beaut.js"):
 
 
        
-# LIST OF FIXES/TWEAKS
 replacements = {'4:"libraryroot"}[n=u]||n': '4:"libraryreet"}[n=u]||n'}
 
-'''
-fixes_dict = {
-    'Child: d,': 'Child: d,',
-    'gridColumnGap: 16,': 'gridColumnGap: 5,',
-    'gridRowGap: 24,': 'gridRowGap: 8,',
-    'name : "library_600x900.jpg", e.rt': 'name : "header.jpg", e.rt',
-    '[$o.GetCachedVerticalCapsuleURL(this.props.app), $o.GetVerticalCapsuleURLForApp(this.props.app),': \
-    '[$o.GetCachedLandscapeImageURLForApp(this.props.app), $o.GetLandscapeImageURLForApp(this.props.app),',
-    't + "/portrait.png?v=2"': 't + "/header.jpg"',
-    'vecScreenShots.slice(0, 4).map': 'vecScreenShots.slice(0, 9).map',
-    't = t.slice(0, 6),': 't = t.slice(0, 12),',
-    'rowHeight: 90,': 'rowHeight: 40,'
-}
-'''
 fixes_dict = {}
 
 #modify library.js to look for different libraryroot.js file
