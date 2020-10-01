85b1118ff4ba3b4331f303911054b82744972611
Jonius7 <jhgenius01@hotmail.com>
2020-10-02 00:05:54 +1000

    testapp switch 3 frames

--- TestApp.py
+++ TestApp.py
@@ -14,12 +14,14 @@ class OldGloryApp(tk.Tk):
 
         self.frames = {}
 
-        frame = StartPage(container, self)
+        for F in (StartPage, PageOne, PageTwo):
 
-        self.frames[StartPage] = frame
+            frame = F(container, self)
 
-        frame.grid(row=0, column=0, sticky="nsew")
-        ###frame.grid(row=110, column=110, sticky="nsew")
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+            ###frame.grid(row=110, column=110, sticky="nsew")
         
         self.show_frame(StartPage)
 
@@ -27,8 +29,8 @@ class OldGloryApp(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
-def qf(param):
-    print(param)
+###def qf(param):
+###    print(param)
 
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -37,8 +39,35 @@ class StartPage(tk.Frame):
         label.pack(pady=10, padx=10)
 
         button1 = tk.Button(self, text="Visit Page 1",
-                            command=lambda: qf("see this worked"))
+                            command=lambda: controller.show_frame(PageOne))
         button1.pack()
+        button2 = tk.Button(self, text="Visit Page 2",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+class PageOne(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page 1", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+        button2 = tk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+class PageTwo(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page 2", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+        button2 = tk.Button(self, text="Page One",
+                            command=lambda: controller.show_frame(PageOne))
+        button2.pack()
 
 app = OldGloryApp()
 app.mainloop()
