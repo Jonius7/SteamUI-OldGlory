119edb73a154d8c88e588ad300c8d2ca62a04527
Jonius7 <jhgenius01@hotmail.com>
2020-10-09 18:03:01 +1000

    Fixed screens not switching weird class missing, new images

--- backend.py
+++ backend.py
@@ -1,5 +1,12 @@
 import winreg
 
+DEFAULT_CONFIG = {"SteamPath" : "",
+                  "InstallCSSTweaks" : "1",
+                  "EnablePlayButtonBox" : "0",
+                  "EnableVerticalNavBar" : "0",
+                  "InstallWithDarkLibrary" : "0"}
+user_config = {}
+
 def find_library_dir():
     key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "SOFTWARE\Valve\Steam");
     steam_path = winreg.QueryValueEx(key, "SteamPath")[0]
--- old_glory.py
+++ old_glory.py
@@ -10,123 +10,225 @@ class OldGloryApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
-        windowW = 600
+        windowW = 700
         windowH = 400
         self.geometry((str(windowW)+'x'+str(windowH)+'+950+400'))
         self.minsize(width=windowW, height=windowH)
         self.maxsize(width=windowW, height=windowH)
-
+        container.pack(side="top", fill="both", expand = True)
+        
         self.iconbitmap('steam_oldglory.ico')
         self.wm_title("SteamUI-OldGlory Configurer")
-        frameTitle = tk.Frame()
+        ###frameTitle = tk.Frame()
 
         ###
         self.default_font = TkFont.nametofont("TkDefaultFont")
-        self.default_font.configure(size=13)
-        
-        ###container.grid(side="top", fill="both", expand = True)
+        self.default_font.configure(size=13)      
 
+        ### Custom Checkbutton layout
+        style=ttk.Style()
+        style.layout("TCheckbutton",
+            [('Checkbutton.padding', {'sticky': 'nswe', 'children':
+                [('Checkbutton.focus', {'side': 'left', 'sticky': '', 'children':
+                    [('Checkbutton.indicator', {'side': 'top', 'sticky': 'nswe'})
+                    ]
+                })]
+            })]
+        )
+        
+        
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
+    
         self.frames = {}
 
         for F in (StartPage, PageOne):
-
             frame = F(container, self)
-
-            self.frames[F] = frame
-
             frame.grid(row=0, column=0, sticky="nsew")
-            ###frame.grid(row=110, column=110, sticky="nsew")
-        
+            self.frames[F] = frame
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
-        
+        self.frames[cont].tkraise()
+
+    def load_from_config():
+        print("yahoo")
 
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         
-        ### HEAD FRAME
-        ###
-        frameHead = tk.Frame()
-        
-        ###
-        labeltext1 = tk.StringVar()
-        labeltext1.set("SteamUI-OldGlory")
-        label1 = tk.Label(frameHead, textvariable=labeltext1, font=(controller.default_font, 20))
-        ###label1.pack(padx=20)
-        label1.grid(row=0, column=0)
-
-        ###
-        labeltext2 = tk.StringVar()
-        labeltext2.set("A set of CSS and JS tweaks for the Steam Library")
-        label2 = tk.Label(frameHead, textvariable=labeltext2)
-        ###label2.pack(padx=20, pady=10)
-        label2.grid(row=1, column=0)
+        frameHead = head_frame(self, controller)
 
 
         ### CHECK FRAME
         ###
-        frameCheck = tk.Frame()
+        frameCheck = tk.Frame(self)
         
         ###
         self.var1 = tk.IntVar()
         check1 = ttk.Checkbutton(frameCheck,
-                                 text="Install CSS Tweaks (SteamUI-OldGlory)",
-                                 variable=self.var1,
+                                 variable=self.var1
                                  )
                                  
         check1.bind("<Button-1>", lambda event:css_cb_check(event, self.var1, check2, check3))
         check1.grid(row=0, column=0)
+        label1 = tk.Label(frameCheck,
+                          text="Install CSS Tweaks (SteamUI-OldGlory)")
+        label1.grid(row=0, column=1, sticky="w")
+
 
         ###
         self.var2 = tk.IntVar()
         check2 = ttk.Checkbutton(frameCheck,
-                                 text="- Box Play Button",
                                  variable=self.var2,
                                  state='disabled')
         check2.grid(row=1, column=0)
+        label2 = tk.Label(frameCheck,
+                          text="  - Box Play Button")
+        label2.bind("<Button-1>", lambda event:change_image(image1, 'play_button_box.png'))
+        label2.grid(row=1, column=1, sticky="w")
         
         ###
-        image1 = open_img(frameCheck, 'buttons_before_after.png')
-        image1.grid(row=0, column=8, rowspan=3)
+        image1 = add_img(frameCheck, 'buttons_before_after.png')
+        image1.grid(row=0, column=2, rowspan=5, padx=5, sticky="n")
         
         ###
         self.var3 = tk.IntVar()
         check3 = ttk.Checkbutton(frameCheck,
-                                 text="- Vertical Nav Bar",
                                  variable=self.var3,
                                  state='disabled')
         check3.grid(row=2, column=0)
+        label3 = tk.Label(frameCheck,
+                          text="  - Vertical Nav Bar")
+        label3.grid(row=2, column=1, sticky="w")
 
         ###
         self.var4 = tk.IntVar()
-        check4 = ttk.Checkbutton(frameCheck, text="Install with Dark Library (steam-library)", variable=self.var4)
-        check4.grid(row=3,column=0)
+        check4 = ttk.Checkbutton(frameCheck,
+                                 variable=self.var4)
+        check4.grid(row=3, column=0)
+        label4 = tk.Label(frameCheck,
+                          text="Install with Dark Library (steam-library)")
+        label4.bind("<Button-1>", lambda event:change_image(image1, 'dark_steam_library.png'))
+        label4.grid(row=3, column=1, sticky="w")
+
+        ###
+        label_end = tk.Label(frameCheck, height=5)
+        label_end.grid(row=4, column=0, columnspan=2)
+        
+
+        ### MODE Frame
+        ###
+        frameMode = tk.Frame(self)
+        self.var_m = tk.IntVar()
+        button_m = ttk.Button(frameMode,
+                           text="Advanced Options",
+                           width=20
+        )
+        ###button_m.bind("<Button-1>", lambda event:controller.show_frame(PageOne))
+        button_m.config(command=lambda: controller.show_frame(PageOne))
+        button_m.grid(row=0, column=0, padx=5)
+
         
         ### CONFIRM FRAME
         ###
-        frameConfirm = tk.Frame()
+        ###frameConfirm = confirm_frame(self.var1, frameHead.labeltext_b)
+
         ###
-        button1 = ttk.Button(frameConfirm,
-                           text="Install",
-                           width=20,                       
-        )
-        button1.bind("<Button-1>", lambda event:handle_click(event, self.var1, labeltext2))
-        button1.grid(column=0)
-        
         frameHead.pack()
         frameCheck.pack()
-        frameConfirm.pack()
+        frameMode.pack(pady=8)
+        ###frameConfirm.pack()
 
 class PageOne(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
+        frameCheck = tk.Frame(self)
+        self.var3 = tk.IntVar()
+        check3 = ttk.Checkbutton(frameCheck,
+                                 variable=self.var3,
+                                 state='disabled')
+        check3.grid(row=2, column=0)
+        label3 = tk.Label(frameCheck,
+                          text="  - Vertical Nav Bar")
+        label3.grid(row=2, column=1, sticky="w")
+        self.var3 = tk.IntVar()
+        check3 = ttk.Checkbutton(frameCheck,
+                                 variable=self.var3,
+                                 state='disabled')
+        check3.grid(row=2, column=0)
+        label3 = tk.Label(frameCheck,
+                          text="  - Vertical Nav Bar")
+        label3.grid(row=2, column=1, sticky="w")
+        self.var3 = tk.IntVar()
+        check3 = ttk.Checkbutton(frameCheck,
+                                 variable=self.var3,
+                                 state='disabled')
+        check3.grid(row=2, column=0)
+        label3 = tk.Label(frameCheck,
+                          text="  - Vertical Nav Bar")
+        label3.grid(row=2, column=1, sticky="w")
+        
+        ### MODE Frame
+        ###
+        frameMode = tk.Frame(self)
+        self.var_m = tk.IntVar()
+        button_m = ttk.Button(frameMode,
+                           text="Options",
+                           width=20
+        )
+        button_m.bind("<Button-1>", lambda event:controller.show_frame(StartPage))
+        button_m.grid(row=0, column=0, padx=5)
+        
+        ###frameConfirm = confirm_frame(tk.IntVar(), tk.StringVar())
+        frameCheck.pack()
+        frameMode.pack(pady=8)
+        ###frameConfirm.pack()
+
+def head_frame(self, controller):
+    ### HEAD FRAME
+    ###
+    frameHead = tk.Frame(self)
+        
+    ###
+    titlefont = controller.default_font.copy()
+    titlefont.configure(size=20)
+        
+    labeltext_a = tk.StringVar()
+    labeltext_a.set("SteamUI-OldGlory")
+        
+    label_a = tk.Label(frameHead, textvariable=labeltext_a, font=titlefont)
+    label_a.grid(row=0, column=0)
+
+    ###
+    labeltext_b = tk.StringVar()
+    labeltext_b.set("A set of CSS and JS tweaks for the Steam Library")
+        
+    label_b = tk.Label(frameHead, textvariable=labeltext_b)
+    label_b.grid(row=1, column=0)
+    return frameHead
+
+def confirm_frame(checked, labeltext):
+    frameConfirm = tk.Frame()
+    ###
+    button1 = ttk.Button(frameConfirm,
+                       text="Install",
+                       width=15                       
+    )
+    button1.bind("<Button-1>", lambda event:install_click(event, checked, labeltext))
+    button1.grid(row=0, column=0, padx=5)
+
+    button2 = ttk.Button(frameConfirm,
+                       text="Load from Config",
+                       width=15,
+                       state='disabled'
+    )
+    button2.bind()
+    button2.grid(row=0, column=1)
+    return frameConfirm
+
 
 def css_cb_check(event, var1, check2, check3):
     if var1.get() == 0:
@@ -137,23 +239,31 @@ def css_cb_check(event, var1, check2, check3):
         check3.config(state='disabled')
 
 
-def handle_click(event, var1, labeltext):
+def install_click(event, var1, labeltext):
     if var1.get() == 1:
         labeltext.set("var enabled " + str(var1.get()))
     else:
         labeltext.set("var disabled " + str(var1.get()))
 
-def open_img(frame, filename):
+def open_img(filename):
     x = filename
     img = Image.open(x)
-    new_width = 250
+    new_width = 350
     new_height = int(new_width * img.height / img.width)
     img = img.resize((new_width, new_height), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img)
+    return img
+
+def add_img(frame, filename):
+    img = open_img(filename)
     panel = tk.Label(frame, image=img)
     panel.image = img
     return panel
 
+def change_image(label, filename):
+    img = open_img(filename)
+    label.configure(image=img)
+    label.image = img
 
 ### Initialisation
    
