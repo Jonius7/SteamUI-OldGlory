860f68dd34e0a6c0803e64fc162b74785a712c5c
Jonius7 <jhgenius01@hotmail.com>
2023-02-04 17:36:06 +1100

    Bump version

--- old_glory.py
+++ old_glory.py
@@ -25,8 +25,8 @@ DEBUG_STDOUT_STDERR = False # Only useful for debugging purposes, set to True
 
 class OldGloryApp(tk.Tk):
     def __init__(self, *args, **kwargs):
-        self.version = "v0.9.27.8"
-        self.release = "5.10.5-pre1"
+        self.version = "v0.9.27.9"
+        self.release = "5.10.5"
       
         ### Window Frame
         tk.Tk.__init__(self, *args, **kwargs)
