7c4e9a01387e34b38fa6435b1602111da06c180b
Jonius7 <jhgenius01@hotmail.com>
2024-05-08 21:38:05 +1000

    Release 5.13 new class mapping, remove defunct JS tweaks

--- old_glory.py
+++ old_glory.py
@@ -25,8 +25,8 @@ DEBUG_STDOUT_STDERR = False # Only useful for debugging purposes, set to True
 
 class OldGloryApp(tk.Tk):
     def __init__(self, *args, **kwargs):
-        self.version = "1.1.1"
-        self.release = "5.12.1"
+        self.version = "1.1.1.1"
+        self.release = "5.13"
       
         ### Window Frame
         tk.Tk.__init__(self, *args, **kwargs)
