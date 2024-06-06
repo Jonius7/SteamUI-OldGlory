5f8a54091f51b2c56e5fe0c4e5b0a58b7019178c
Jonius7 <jhgenius01@hotmail.com>
2024-06-06 11:31:17 +1000

    Update RUN_old_glory.bat (variablify it)

--- RUN_old_glory.bat
+++ RUN_old_glory.bat
@@ -1,2 +1,5 @@
 @echo off
-"C:\Users\Jason\Downloads\SteamUI-OldGloryMaster\.env\Scripts\python.exe" old_glory.py
+set oldglorydir=SteamUI-OldGlory
+set venvdir=venv
+
+"%USERPROFILE%\Downloads\%oldglorydir%\%venvdir%\Scripts\python.exe" old_glory.py
