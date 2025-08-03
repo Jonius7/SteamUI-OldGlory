99b0fe3745e95ca33333d9bd5093825b940fcf14
Jonius7 <jhgenius01@hotmail.com>
2025-08-03 18:03:30 +1000

    Remove Legacy Files/Snippets

--- src/scss_compile.py
+++
@@ -1,8 +0,0 @@
-import sass
-import os
-#os.mkdir('css')
-#os.mkdir('sass')
-
-#compile sass
-sass.compile(dirname=('scss','.'),
-             output_style='expanded')
--- src/subprocess_create.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-CREATE_NO_WINDOW = 0x08000000
-def run():
-    print(str(subprocess.check_output('tasklist', creationflags=CREATE_NO_WINDOW)))
